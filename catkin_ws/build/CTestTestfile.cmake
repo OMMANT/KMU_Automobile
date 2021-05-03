b5c5ac265e60bbfebe2641b4a0dadf4095955474
automobile <ommant@naver.com>
2021-05-03 04:24:53 -0700

    Initial commit for starting assignment 1

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ommant/catkin_ws/src
+# Build directory: /home/ommant/KMU_Automobile/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("xycar_sim_drive")
