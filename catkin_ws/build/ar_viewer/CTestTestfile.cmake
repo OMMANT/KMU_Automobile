b117cbb92024b7d1389dd014deb7d9a608e50a94
automobile <ommant@naver.com>
2021-05-03 04:56:46 -0700

    Initial commit for starting assignment3

---
+++ catkin_ws/build/ar_viewer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ommant/catkin_ws/src/ar_viewer
+# Build directory: /home/ommant/KMU_Automobile/catkin_ws/build/ar_viewer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
