3975f965f917b0a4f20ae5ee0a5db3f760971623
KMU <ommant@naver.com>
2021-05-04 07:56:52 -0700

    Assignment 1 Tutorial

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,8 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ommant/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ommant/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/home/ommant/KMU_Automobile/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/ommant/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/ommant/KMU_Automobile/catkin_ws/src:/home/ommant/catkin_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
+++ catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
@@ -3,7 +3,17 @@
 import rospy, math
 from std_msgs.msg import Int32MultiArray
 
+# Data Store Order (_ is discarded)
+# (front left, front middle, fornt right, _, _, _, right, left)
+fl, fm, fr, r, l = 0, 0, 0, 0, 0
+before_data = (0, 0, 0, 0, 0)
+
+angle = 0
+speed = 10
 def callback(msg):
+    global fl, fm, fr, r, l     # Use global variable fl, fm, fr, r, l
+    fl, fm, fr, _, _, _, r, l = msg.data
+    # Without global keyword, assign the value at local variables fl, fm, fr, r, l
     print(msg.data)
 
 rospy.init_node('guide')
@@ -13,6 +23,19 @@ ultra_sub = rospy.Subscriber('ultrasonic', Int32MultiArray, callback)
 xycar_msg = Int32MultiArray()
 
 while not rospy.is_shutdown():
-    angle = 0
-    xycar_msg.data = [angle, 10]
+    # ===== Code Write ===== #
+    if fm > 400: # If there is no obstacle at front middle, speed up
+        speed = 30
+    elif fm > 200: # Obstacle distance over 200 and less than 400, speed down
+        speed = 15
+    else: # Obstacle close, speed down
+        speed = 10
+    if fr > 200:
+        angle = 20
+    else:
+        angle = 0
+    # ===== ======== ===== #
+    # xycar data set, [angle, speed]
+    xycar_msg.data = [angle, speed]
     motor_pub.publish(xycar_msg)
+    
